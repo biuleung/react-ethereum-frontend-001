4743f720d90ea2435d78f2b3b9f9638bf0cd2b2c
biuleung <biuleong@gmail.com>
2022-06-12 23:11:35 +0800

    Updates

--- static/js/6.3e9dd175.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-ethereum-frontend-001"]=this["webpackJsonpreact-ethereum-frontend-001"]||[]).push([[6],{777:function(e,t,r){"use strict";r.r(t);r(2);var n=r(27);t.default=function(){return Object(n.jsx)(n.Fragment,{children:Object(n.jsx)("div",{children:"Dashboard works"})})}}}]);
-//# sourceMappingURL=6.3e9dd175.chunk.js.map
